80651757167b82f6f81f29594a5ff5a77c5b1b4d
Giga Goriashvili <g.goriashvili12@gmail.com>
2025-04-24 21:00:40 +0400

    feat(web-ui): initialize React project with TypeScript

---
+++ web-ui/src/App.tsx
@@ -0,0 +1,7 @@
+import { Home } from './pages/HOME';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ web-ui/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+// src/pages/Home.tsx
+export function Home() {
+    return <h1>Hello from Home Page!</h1>;
+  }
+  
